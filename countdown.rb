c7eba55b75f2eaddd7a81ccb1ea38eabd1f2a700
Kevawn Warren <kevawnw@gmail.com>
2022-04-05 16:57:54 -0400

    commit tree 01419b1f8ec14817c4aa5fa6d50652ead4285d30

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown int
+    while 1 <= int
+       puts "#{int} SECOND(S)!"
+       count_with_sleep(5)
+        int -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def count_with_sleep(slp)
+    sleep(slp)
 end
